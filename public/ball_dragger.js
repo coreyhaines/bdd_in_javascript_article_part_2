4f16d2c6e2525e4ff75c067a5d28cb023fb2a399
coreyhaines <coreyhaines@gmail.com>
2010-09-27 13:09:34 -0500

    Finished

--- public/ball_dragger.js
+++ public/ball_dragger.js
@@ -1,21 +1,58 @@
-
-
-
-
 var BallDragger = {
   initialize: function(gameBoard) {
-    var field, startBox, startBoxPosition, ball;
+    var field, startBox, startBoxPosition,
+        destinationBox, destinationBoxPosition,
+        resultDisplay, ball, game;
     field = gameBoard.find("#field");
+
     startBox = field.find("#ball-start");
     startBoxPosition = startBox.position();
-    ball = $("<div id='ball'></div>").draggable({containment:"#field"});
 
-    return {
+    destinationBox = field.find("#ball-destination");
+
+    ball = $("<div id='ball'></div>");
+    resultDisplay = gameBoard.find("#results");
+
+
+    function inside(element, thisContainer) {
+      var elementPosition, thisContainerPosition;
+
+      thisContainerPosition = thisContainer.position();
+      elementPosition = element.position();
+
+      console.log(elementPosition);
+      console.log(thisContainerPosition);
+
+      inside = (elementPosition.left >= thisContainerPosition.left) &&
+      (elementPosition.left <= thisContainerPosition.left + thisContainer.width()) &&
+      (elementPosition.top >= thisContainerPosition.top) &&
+      (elementPosition.top <= thisContainerPosition.top + thisContainer.height());
+
+      return inside;
+    }
+
+    function hasWon() {
+      return inside(ball, destinationBox);
+    }
+
+    game = {
       start: function() {
         ball.appendTo(field);
         ball.css("left", startBoxPosition.left);
         ball.css("top", startBoxPosition.top);
+      },
+      ballDropped: function() {
+        if(hasWon()){
+          resultDisplay.text("You Win!");
+        }else{
+          resultDisplay.text("You Lose");
+        }
       }
     };
+
+    ball.draggable({containment:"#field",
+                  stop: game.ballDropped});
+
+    return game;
   }
 };
--- specs/start.spec.js
+++ specs/start.spec.js
@@ -16,7 +16,7 @@ describe("BallDragger#start", function() {
   afterEach(function(){
     gameBoard.replaceWith(originalGameBoardStructure);
   });
-  
+
   describe("with no ball on field", function() {
     it("creates a ball in the start box", function() {
       var ball, startBox;
@@ -26,6 +26,59 @@ describe("BallDragger#start", function() {
       startBox = gameBoard.find("#field #ball-start");
 
       expect(ball).toBeInside(startBox);
+    });
+  });
+});
+
+describe("BallDragger#dropped", function(){
+  var gameBoard, originalGameBoardStructure, ballDraggerGame;
+
+  beforeEach(function() {
+    originalGameBoardStructure = $("#game");
+    gameBoard = originalGameBoardStructure.clone();
+    originalGameBoardStructure.replaceWith(gameBoard);
+
+    ballDraggerGame = BallDragger.initialize(gameBoard);
+  });
+
+  afterEach(function(){
+    gameBoard.replaceWith(originalGameBoardStructure);
+  });
+
+  describe("ball is in the destination", function() {
+    it("sets the results display to 'You Win'", function() {
+      var ball, destinationBox, resultDisplay;
+      ballDraggerGame.start();
+
+      ball = gameBoard.find("#field #ball");
+      destinationBox = gameBoard.find("#field #ball-destination");
+
+      ball.css("left", destinationBox.position().left + 2);
+      ball.css("top", destinationBox.position().top + 2);
+
+      ballDraggerGame.ballDropped();
+
+      resultDisplay = gameBoard.find("#results");
+      expect(resultDisplay.text()).toEqual("You Win!");
+    });
+  });
+
+  describe("ball is not in the destination", function() {
+    it("sets the results display to 'You Lose'", function() {
+      var ball, notDestinationBox, resultDisplay;
+
+      ballDraggerGame.start();
+
+      ball = gameBoard.find("#field #ball");
+      notDestinationBox = gameBoard.find("#field #ball-not-destination");
+
+      ball.css("left", notDestinationBox.position().left + 2);
+      ball.css("top", notDestinationBox.position().top + 2);
+
+      ballDraggerGame.ballDropped();
+
+      resultDisplay = gameBoard.find("#results");
+      expect(resultDisplay.text()).toEqual("You Lose");
     });
   });
 });
